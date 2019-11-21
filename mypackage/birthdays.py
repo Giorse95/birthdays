2b66573e0bdaba7e865a43eae0d5c318f4d445e6
Giorse95 <giorgio.torselloo@gmail.com>
2019-11-21 15:18:52 +0100

    Properly document the Code

--- mypackage/birthdays.py
+++ mypackage/birthdays.py
@@ -1,3 +1,6 @@
+
+""" The goal of this module is to have a list of people with their respective dates of birthdays """
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -5,12 +8,17 @@ birthdays = {
     'Donald Trump': '06/14/1946',
     'Rowan Atkinson': '01/6/1955'}
 
+
+""" It prints all the names of the people contained in our Database """
+
 def print_birthdays():
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
-def return_birthday(name):
+""" If we have info about him/her, it prints the name of that person and its respective date of birth; if not it prints Sadly we don't have that birthday """
+
+def return_birthday(name)
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
