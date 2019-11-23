2d7cffd780b056e79546ed335a97198d06f37481
LaAliss <alice.rigotto@student.h-farm.com>
2019-11-23 04:47:10 -0500

    Modified with sys

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python3
 
+import sys
 from mypackage import birthdays
 
-birthdays.return_birthday('Albert Einstein')
-birthdays.return_birthday('Alan Turing')
+if len(sys.argv)>1:
+    birthdays.return_birthday(sys.argv[1])
+else:
+    print("Please type a valid name")
+
+""" remember that every time you will type a name, if they are two different words " " are necessary """
